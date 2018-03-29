aa13f0bd51c06f8ce45911df3cb7e1f61921ddfb
Spencer Croy <spencerccroy@gmail.com>
2018-03-29 19:54:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ function divide(a, b) {
 }
 
 function inc(n) {
-  return (parseInt(n) += 1)
+  return parseInt(n) += 1
 }
 
 function dec(n) {
-  return (parseInt(n) -= 1)
+  return parseInt(n) -= 1
 }
